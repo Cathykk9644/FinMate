6b1c402e763ca363a5e72ecf62fc32c134accb2d
Cathy Hui <erashine414@icloud.com>
2024-06-14 17:52:40 +0800

    Implement RightSideBar of the Home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,20 @@
 import BalanceBox from "@/components/BalanceBox";
 import HeaderBox from "@/components/HeaderBox";
+import RightSideBar from "@/components/RightSideBar";
 import React from "react";
 
 const HomePage = () => {
-  const loggedIn = { firstName: "Cathy", lastName: "Hui" };
+  const loggedIn = {
+    firstName: "Cathy",
+    lastName: "Hui",
+    email: "contact@gmail.com",
+  };
   return (
     <section className="home">
       <div className="home-content">
         <HeaderBox
           type="greeting"
-          title="👋 😉 Welcome"
+          title="👋 👋 Welcome"
           user={loggedIn?.firstName || "Guest"}
           subtext="Access and manage your personal account and transaction effectively."
         />
@@ -18,7 +23,13 @@ const HomePage = () => {
           totalBanks={1}
           totalCurrentBalance={49966.19}
         />
+        Recent Transaction
       </div>
+      <RightSideBar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 1644.91 }, { currentBalance: 644.94 }]}
+      />
     </section>
   );
 };
