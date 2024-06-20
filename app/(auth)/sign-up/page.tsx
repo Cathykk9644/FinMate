ae4e80f45fbd0744c969159381d5d15c3ef4b6fa
Cathy Hui <erashine414@icloud.com>
2024-06-20 16:06:28 +0800

    implement form UI for signup  & signin pges

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,7 +1,12 @@
+import AuthForm from "@/components/AuthForm";
 import React from "react";
 
 const SignUpPage = () => {
-  return <div>SignUpPage</div>;
+  return (
+    <section className="flex-center size-full max-sm:px-6">
+      <AuthForm type="sign-up" />
+    </section>
+  );
 };
 
 export default SignUpPage;
